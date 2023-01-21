e9ae73d3b07473ea9f6d3cd35356ccc49eb24f10
Richard Julian <richard@rjulian.net>
2023-01-21 15:03:26 -0800

    Add in support for multiple trigger types

--- {{cookiecutter.function_name}}/terraform/function.tf
+++ {{cookiecutter.function_name}}/terraform/function.tf
@@ -3,16 +3,24 @@ resource "google_cloudfunctions_function" "function" {
   description           = "{{cookiecutter.function_description}}"
   {% if cookiecutter.gcp_python_runtime == "3.11" %}
   runtime               = "python311"
-  {% if cookiecutter.gcp_python_runtime == "3.10" %}
+  {% elif cookiecutter.gcp_python_runtime == "3.10" %}
   runtime               = "python310"
-  {% if cookiecutter.gcp_python_runtime == "3.9" %}
+  {% elif cookiecutter.gcp_python_runtime == "3.9" %}
   runtime               = "python39"
-  {% if cookiecutter.gcp_python_runtime == "3.8" %}
+  {% elif cookiecutter.gcp_python_runtime == "3.8" %}
   runtime               = "python38"
-  {% if cookiecutter.gcp_python_runtime == "3.7" %}
+  {% elif cookiecutter.gcp_python_runtime == "3.7" %}
   runtime               = "python37"
   {% endif %}
+  {% if cookiecutter.function_trigger == "http" %}
   entry_point           = "hello_world"
+  {% elif cookiecutter.function_trigger == "cloud storage" %}
+  entry_point           = "hello_gcs"
+  {% elif cookiecutter.function_trigger == "pub/sub" %}
+  entry_point           = "hello_pubsub"
+  {% elif cookiecutter.function_trigger == "firestore" %}
+  entry_point           = "hello_firestore"
+  {% endif %}
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.object.name
 
@@ -20,5 +28,5 @@ resource "google_cloudfunctions_function" "function" {
   service_account_email = google_service_account.sa.email
   available_memory_mb   = 256
 
-  trigger_http = true
+  function_trigger_http = true
 }
--- {{cookiecutter.function_name}}/{{cookiecutter.function_name}}/main.py
+++ {{cookiecutter.function_name}}/{{cookiecutter.function_name}}/main.py
@@ -1,5 +1,5 @@
 """Placeholder python code for use in Google Cloud Function"""
-
+{% if cookiecutter.function_trigger == "http" %}
 def hello_world(request):
     """Responds to any HTTP request.
     Args:
@@ -15,3 +15,40 @@ def hello_world(request):
     if request_json and "message" in request_json:
         return request_json["message"]
     return "Hello World!"
+
+{% elif cookiecutter.function_trigger == "cloud storage" %}
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+
+{% elif cookiecutter.function_trigger == "pub/sub" %}
+import base64
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
+
+{% elif cookiecutter.function_trigger == "firestore" %}
+def hello_firestore(event, context):
+    """Triggered by a change to a Firestore document.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    resource_string = context.resource
+    # print out the resource string that triggered the function
+    print(f"Function triggered by change to: {resource_string}.")
+    # now print out the entire event object
+    print(str(event))
+
+{% endif %}
